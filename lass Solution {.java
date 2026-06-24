b31804b19dfad251eb8380226336324dd58531f5
Sukanya Beuria <sukanyabeuriasukanya@gmail.com>
2026-06-24 10:35:53 +0530

    solved longest common prefix

--- lass Solution {.java
+++ lass Solution {.java
@@ -1,4 +1,4 @@
-lass Solution {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i =0 ; i < nums.length; i ++){
             for (int j = i+1 ; j < nums.length; j++){
