cacae984e908d70ace0f6da4ed4309c02f960221
Sukanya Beuria <sukanyabeuriasukanya@gmail.com>
2026-06-26 19:27:56 +0530

    Postive Number And Negative Number

---
+++ conditional/posNeg.java
@@ -0,0 +1,18 @@
+
+
+public class posNeg {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        System.out.println("Enter a number:");
+        n = scanner.nextInt();
+        if (n >0){
+            System.out.println("The number is postive ");
+            else if (n<0){
+                System.out.println("The number is negative number");
+            }else{
+                System.out.println("The number is Zero");
+            }
+            }
+        }
