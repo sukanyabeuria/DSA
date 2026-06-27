238f1eb325a619523d64df86372afd0625e58efc
Sukanya Beuria <sukanyabeuriasukanya@gmail.com>
2026-06-27 20:43:05 +0530

    print all natural numbers from 1 to n

---
+++ conditional/loop/naturalNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Natural_Number
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Starting Number : ");
+		int s =input.nextInt();
+		System.out.print("Enter The Ending Number : ");
+		int e =input.nextInt(); 
+		while(s<=e)
+		{
+			System.out.println(s);
+			s++;
+		}
+	}
+}
