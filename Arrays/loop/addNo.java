73959540ffaedb3fe6251c32d4acd48a1cc8e65e
Sukanya Beuria <sukanyabeuriasukanya@gmail.com>
2026-06-28 20:50:09 +0530

    Odd number  from 1 to 100

---
+++ Arrays/loop/addNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Odd_Numbers
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Number of Limit : ");
+		int l =input.nextInt();
+		for(int s=1;s<=l;s++)
+		{
+			if(s%2==1)
+				System.out.println(s);
+		}
+	}
+}
